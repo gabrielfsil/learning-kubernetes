07359e19e437ef29404ce1b606b37edeceda4b37
Gabriel Fernandes <gabrielfsil264@gmail.com>
2024-06-10 01:47:17 -0300

    feat(observabilty): adicionando grafana e prometheus

--- consumer/src/shared/containers/MailProvider/implementations/EtherealProvider.ts
+++ consumer/src/shared/containers/MailProvider/implementations/EtherealProvider.ts
@@ -1,9 +1,53 @@
+import {TestAccount, Transporter} from 'nodemailer';
 import {ISendMailDTO} from '../dtos/ISendMailDTO';
 import {IMailProvider} from '../model/IMailProvider';
+const nodemailer = require('nodemailer');
 
 class EtherealProvider implements IMailProvider {
+  private client: Transporter | undefined;
+  constructor() {
+    nodemailer.createTestAccount().then((account: TestAccount) => {
+      const transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false,
+        tls: {
+          rejectUnauthorized: true,
+          minVersion: 'TLSv1.2',
+        },
+        auth: {
+          user: account.user,
+          pass: account.pass,
+        },
+      });
+
+      this.client = transporter;
+    });
+  }
   public async send({to, from, subject, body}: ISendMailDTO): Promise<void> {
-    throw new Error('Method not implemented.');
+    try {
+      if (this.client) {
+        const info = await this.client.sendMail({
+          from: {
+            name: from.name,
+            address: from.email,
+          },
+          to: {
+            address: to.email,
+            name: to.name,
+          },
+          subject,
+          text: body,
+        });
+        console.log('Message ID %s', info.messageId);
+        console.log('%s', nodemailer.getTestMessageUrl(info));
+      } else {
+        throw new Error('Client not initialized');
+      }
+    } catch (err) {
+      console.log(err);
+      throw new Error('Client not initialized');
+    }
   }
 }
 
